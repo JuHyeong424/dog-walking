b20d9ba1312cabf70faa86fec50112353758917f
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-19 01:54:55 +0900

    feat: header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import "@/app/globals.css";
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en">
     <body>
-    <header>
-      <nav>
+    <header className="flex items-center justify-end h-30 bg-blue-200 p-4">
+      <nav className="text-white font-bold p-4">
         <Link href="/">Home</Link> | <Link href="/about">About</Link> | <Link href="/login">Login</Link> |
         <Link href="/profile">Profile</Link> | <Link href="/walking">Walking</Link>
       </nav>
