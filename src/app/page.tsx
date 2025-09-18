c341376aba89f92cc896e5f1c0c01feb582a6015
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-19 01:57:56 +0900

    refactor: home setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div>
-      hello
+      <div>오늘의 날씨 + 산책지수</div>
+      <div>경로 그리기 버튼</div>
+      <div>추천 용품</div>
+      <div>추천 경로</div>
     </div>
   );
 }
