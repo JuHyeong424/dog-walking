d75a4a9430b06f3709c0fb2a8f0a7d4d07c19f86
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-19 21:55:49 +0900

    style: bg

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -2,12 +2,8 @@ import WeatherComponent from "@/app/weather/components/WeatherComponent";
 
 export default function WeatherPage() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <WeatherComponent />
-      <div>오늘의 날씨 + 산책지수</div>
-      <div>경로 그리기 버튼</div>
-      <div>추천 용품</div>
-      <div>추천 경로</div>
     </div>
   )
 }
