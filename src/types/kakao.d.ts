109690a1d016c6cc327ca13411c94c36ffaef85d
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-09-22 22:08:57 +0900

    refacotr: type & comment

--- src/types/kakao.d.ts
+++ src/types/kakao.d.ts
@@ -7,14 +7,23 @@ declare global {
 
   namespace kakao.maps {
     class LatLng {
-      constructor(lat: number | undefined, lng: number | undefined);
+      constructor(lat: number, lng: number);
+      getLat(): number;
+      getLng(): number;
+    }
+
+    class Point {
+      constructor(x: number, y: number);
+      x: number;
+      y: number;
     }
 
     class Map {
-      constructor(container: HTMLDivElement | null, options: kakao.maps.MapOptions);
+      constructor(container: HTMLElement, options: MapOptions);
+      setCenter(latlng: LatLng): void;
+      getCenter(): LatLng;
       setLevel(level: number, options?: { animate?: boolean; duration?: number }): void;
       getLevel(): number;
-      setCenter(latlng: LatLng): void;
       addControl(control: MapTypeControl | ZoomControl, position: ControlPosition): void;
     }
 
@@ -24,17 +33,32 @@ declare global {
       setPosition(latlng: LatLng): void;
     }
 
-    class MapTypeControl {
-      constructor();
+    class Polyline {
+      constructor(options: PolylineOptions);
+      setMap(map: Map | null): void;
+      getPath(): LatLng[];
+      setPath(path: LatLng[] | LatLng[][]): void;
+      getLength(): number;
     }
 
-    class ZoomControl {
-      constructor();
+    class CustomOverlay {
+      constructor(options: CustomOverlayOptions);
+      setMap(map: Map | null): void;
+      setPosition(position: LatLng): void;
+      setContent(content: string | HTMLElement): void;
     }
 
-    interface MarkerOptions {
-      position: LatLng;
-      map?: Map;
+    class MapTypeControl { constructor(); }
+    class ZoomControl { constructor(); }
+
+    namespace event {
+      function addListener(target: object, type: string, handler: (event: MouseEvent) => void): void;
+      function removeListener(target: object, type: string, handler: (event: MouseEvent) => void): void;
+
+      interface MouseEvent {
+        latLng: LatLng;
+        point: Point; // 'any' 대신 'Point' 타입으로 지정
+      }
     }
 
     interface MapOptions {
@@ -42,15 +66,31 @@ declare global {
       level: number;
     }
 
+    interface MarkerOptions {
+      position: LatLng;
+      map?: Map;
+    }
+
+    interface PolylineOptions {
+      map?: Map;
+      path: LatLng[] | LatLng[][];
+      strokeWeight?: number;
+      strokeColor?: string;
+      strokeOpacity?: number;
+      strokeStyle?: 'solid' | 'shortdash' | 'shortdot' | 'shortdashdot' | 'shortdashdotdot' | 'dot' | 'dash' | 'dashdot' | 'longdash' | 'longdashdot' | 'longdashdotdot';
+    }
+
+    interface CustomOverlayOptions {
+      map?: Map;
+      content: string | HTMLElement;
+      position: LatLng;
+      xAnchor?: number;
+      yAnchor?: number;
+      zIndex?: number;
+    }
+
     enum ControlPosition {
-      TOP,
-      TOPLEFT,
-      TOPRIGHT,
-      LEFT,
-      RIGHT,
-      BOTTOM,
-      BOTTOMLEFT,
-      BOTTOMRIGHT,
+      TOP, TOPLEFT, TOPRIGHT, LEFT, RIGHT, BOTTOM, BOTTOMLEFT, BOTTOMRIGHT,
     }
 
     function load(callback: () => void): void;
